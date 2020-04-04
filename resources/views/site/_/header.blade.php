c2596bb1f8711df03ce47c58cc3a9521055e1ae6
Rayssa Maria Santos Moreira <rayssa.moreira@csktech.com.br>
2020-04-04 17:14:25 -0300

    header, conteudo, footer

--- resources/views/site/_/header.blade.php
+++ resources/views/site/_/header.blade.php
@@ -1,8 +1,18 @@
-<header>
-        <nav class="navbar navbar-light bg-light">
-           <a class="navbar-brand" href="#">
-           <img class="logo" src="images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-    Bootstrap
-            </a>
-        </nav>
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand d-flex align-items-center" href="/">
+            <img class="logo mr-3" src="images/logo.png" class="d-inline-block align-top" alt="">
+            IEQ Ayrton Senna
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home</a>
+                <a class="nav-item nav-link" href="#">Features</a>
+                <a class="nav-item nav-link" href="#">Pricing</a>
+            </div>
+        </div>
+    </nav>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                 Rayane index
+                 Conteudo
             </div>
         </div>
     </div>
