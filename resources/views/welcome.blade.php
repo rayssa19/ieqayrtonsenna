c82050f961d42e69e32f66e9671d70b7ada8bcc5
Rayssa Maria Santos Moreira <rayssa.moreira@csktech.com.br>
2020-04-05 21:38:09 -0300

    section symbols

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,35 @@
 @section('content')
     <div class="flex-center position-ref full-height">
         <div class="section-home">
-            <div class="background">
+            <div class="d-flex justify-content-center align-items-center background">
+                <div class="row-text">
+                    <h1 class="text-center p-4">Igreja do Evangelho Quadrangular</h1>
+                    <span></span>
+                    <p class="text-center p-4">68 anos no Brasil</p>
+                </div>
+            </div>
+        </div>
+        <div class="section-symbols">
+            <div class="row">
+                <div class="col-md-3 symbols">
+                    <img src="../images/jesus-salva.png" alt="">
+                    Jesus Salva
+                </div>
 
+                <div class="col-md-3 symbols">
+                    <img src="../images/jesus-batiza.png" alt="">
+                    Jesus batiza
+                </div>
+
+                <div class="col-md-3 symbols">
+                    <img src="../images/jesus-cura.png" alt="">
+                    Jesus cura
+                </div>
+
+                <div class="col-md-3 symbols">
+                    <img src="../images/jesus-voltara.png" alt="">
+                    Jesus voltará
+                </div>
             </div>
         </div>
     </div>
