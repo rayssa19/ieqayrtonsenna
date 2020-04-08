62396716565019b15f6b096920f6d84b7cc49da8
Rayssa Maria Santos Moreira <rayssa.moreira@csktech.com.br>
2020-04-07 22:15:26 -0300

    formulario de cadastro

--- resources/views/register-leader.blade.php
+++ resources/views/register-leader.blade.php
@@ -1,5 +1,41 @@
 @extends('layouts.site')
 @section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="section-home">
+            <div class="container">
+                <form>
+                    <div class="form-group">
+                        <label for="name">Célula:</label>
+                        <input type="name" class="form-control" id="name" placeholder="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Nome:</label>
+                        <input type="name" class="form-control" id="name" placeholder="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Timóteo:</label>
+                        <input type="name" class="form-control" id="name" placeholder="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Anfitriã(o):</label>
+                        <input type="name" class="form-control" id="name" placeholder="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Secretário(a):</label>
+                        <input type="name" class="form-control" id="name" placeholder="">
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn">ENVIAR</button>
+                    </div>
 
 
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
