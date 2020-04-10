ac2e1ad43571f86a949436a66f7665db4a3ac08b
Rayssa Maria Santos Moreira <rayssa.moreira@csktech.com.br>
2020-04-10 19:34:28 -0300

    ajustes na home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,22 +13,22 @@
         <div class="section-symbols mt-5">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="col-md-3 symbols red">
+                    <div class="col-lg-3 col-md-6 symbols red">
                         <img class="img-symbols" src="../images/jesus-salva.png" alt="">
                         <p class="text-center font-weight-bold mt-3">Jesus Salva</p>
                     </div>
 
-                    <div class="col-md-3 symbols yellow">
+                    <div class="col-lg-3 col-md-6 symbols yellow">
                         <img class="img-symbols" src="../images/jesus-batiza.png" alt="">
                         <p class="text-center font-weight-bold mt-3">Jesus batiza</p>
                     </div>
 
-                    <div class="col-md-3 symbols blue">
+                    <div class="col-lg-3 col-md-6 symbols blue">
                         <img class="img-symbols" src="../images/jesus-cura.png" alt="">
                         <p class="text-center font-weight-bold mt-3">Jesus cura</p>
                     </div>
 
-                    <div class="col-md-3 symbols purple">
+                    <div class="col-lg-3 col-md-6 symbols purple">
                         <img class="img-symbols" src="../images/jesus-voltara.png" alt="">
                         <p class="text-center font-weight-bold mt-3">Jesus voltará</p>
                     </div>
